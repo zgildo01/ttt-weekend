ea2138b60b61e07469a0df6f7aeff468dab3216e
zgildo01 <zzaragozagildo@gmail.com>
2022-11-30 17:07:42 -0500

    complete code overhaul

--- js/app.js
+++ js/app.js
@@ -1,107 +1,89 @@
-/*-------------------------------- Constants --------------------------------*/
-const winningCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-]
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
+const startBtn = document.querySelector("#startBtn")
+
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
 
 
-/*---------------------------- Variables (state) ----------------------------*/
-let board, turn, winner;
+restartBtn.addEventListener("click", restartGame);
+startBtn.addEventListener("click", initializeGame)
 
 
-/*------------------------ Cached Element References ------------------------*/
-const squareEls = document.querySelectorAll('.boardSq');
-const messageEl = document.querySelector('#message');
-const resetBtnEl = document.querySelector('#reset-button');
-const startBtnEl = document.querySelector("#start-button");
 
-/*----------------------------- Event Listeners -----------------------------*/
-squareEls.forEach(function(square) {
-  square.addEventListener('click', handleClick)
-});
-startBtnEl.addEventListener('click', init);
-resetBtnEl.addEventListener('click', reset);
-
-/*-------------------------------- Functions --------------------------------*/
-
-function init() {
-  board = [null, null, null, null, null, null, null, null, null];
-  turn = 1;
-  winner = null;
-
-  resetBtnEl.removeAttribute("hidden");
-  startBtnEl.style.display = "none";
-  render();
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
 }
 
-function reset() {
-  board = [null, null, null, null, null, null, null, null, null];
-  turn = 1;
-  winner = null;
+function cellClicked(){
+  const cellIndex = this.getAttribute("cellIndex");
+  if(options[cellIndex] != "" || !running){
+      return;
+    }
 
-  resetBtnEl.style.display = "none";
-  startBtnEl.style.display = "initial";
-  render();
+  updateCell(this, cellIndex);
+  checkWinner();
 }
 
-function render() {
-  board.forEach(function(element, idx) {
-    const currentSquare = squareEls[idx];
-
-  if(element === 1) {
-    currentSquare.textContent = 'X';
-  } else if(element === -1) {
-    currentSquare.textContent = 'O';
-  } else if(element === null) {
-    currentSquare.textContent = '';
-  }
-  })
-
-  if(!winner) {
-    messageEl.textContent = "Currently: " + player() + "'s turn!"
-  } else if (winner === 'T') {
-    messageEl.textContent = `Players have tied!`;
-  } else {
-    messageEl.textContent = "Congratulations, Player " + player() + " has won!"
-  }
-
-  getWinner();
+function updateCell(cell, index){
+  options[index] = currentPlayer;
+  cell.textContent = currentPlayer;
 }
 
-function handleClick(evt) {
-  let sqIdx = parseInt(evt.target.id.replace('sq', ''))
-  turn *= -1;
-  board[sqIdx] = turn;
-  winner = getWinner();
-  render();
+function changePlayer(){
+  currentPlayer = (currentPlayer == "X") ? "O" : "X";
+  statusText.textContent = `${currentPlayer}'s turn`;
 }
 
-function player() {
-  let playerName;
-  if (turn === 1) {
-    playerName = 'Player O';
-  } else if(turn === -1) {
-    playerName = 'Player X';
-  }
-  return playerName;
+function checkWinner(){
+    let roundWon = false;
+    for(let i = 0; i < winConditions.length; i++){
+      const condition = winConditions[i];
+      const cellA = options[condition[0]];
+      const cellB = options[condition[1]];
+      const cellC = options[condition[2]];
+
+      if(cellA == "" || cellB == "" || cellC == ""){
+          continue;
+        }
+      if(cellA == cellB && cellB == cellC){
+          roundWon = true;
+          break;
+        }
+    }
+
+    if(roundWon){
+      statusText.textContent = `${currentPlayer} wins!`;
+      running = false;
+    }
+    else if(!options.includes("")){
+      statusText.textContent = `Draw!`;
+      running = false;
+    }
+    else{
+      changePlayer();
+    }
 }
 
-function getWinner() {
-  for(let i = 0; i < winningCombos.length; i++) {
-    let sum = board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]];
-
-    if (Math.abs(sum) === 3) return board[winningCombos[i][0]];
-  }
-
-  if(!board.includes(null)) {
-    return 'T';
-  } else {
-    return;
-  }
+function restartGame(){
+  currentPlayer = "X";
+  options = ["", "", "", "", "", "", "", "", ""];
+  statusText.textContent = `${currentPlayer}'s turn`;
+  cells.forEach(cell => cell.textContent = "");
+  running = true;
 }
